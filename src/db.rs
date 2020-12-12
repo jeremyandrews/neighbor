2e473bae7d49e94d8e58615434f33f606216eb53
Jeremy Andrews <jeremy@tag1.com>
2020-12-12 15:14:11 +0100

    poc for person endpoint

---
+++ src/db.rs
@@ -0,0 +1,11 @@
+/// Functions to help with managing the backend database.
+/// 
+/// @TODO: explore making this database agnostic, supporting at least
+/// MySQL and SQLite in addition to Postgres.
+use sqlx::postgres::PgPool;
+
+/// Connect to the database.
+pub async fn connect() -> anyhow::Result<PgPool> {
+    // @TODO useful error handling
+    Ok(PgPool::connect(&dotenv::var("DATABASE_URL")?).await?)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
+mod db;
+
 use std::io;
+
+use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use sitter::person;
+
+/// POC: list all persons defined in the Sitter database.
+/// @TODO: optionally allow filtering to limit to 1 or more persons.
+async fn read_person(_req: HttpRequest) -> String {
+    let db = db::connect().await.unwrap();
+
+    let persons = person::read(&db, None).await;
+    format!("persons: {:#?}", persons)
+}
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    // @TODO: Make server configurable.
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
+    let listener = "127.0.0.1:5335";
+    println!("Neighbor listening at: {:?}", listener);
 
-    println!("Hello, world!");
-    Ok(())
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            // expose API person endpoint
+            // @TODO: make endpoint paths easily configurable
+            .service(web::resource("/api/person").to(read_person))
+    })
+    .bind(listener)?
+    .run()
+    .await
 }
